71d5b5a07a1a619d0c4a120f95d3dcbcd8e9e1e1
Amaan Qureshi <git@amaanq.com>
2025-12-29 16:39:15 -0500

    sound: add volume panel customization UI

---
+++ src/com/android/settings/sound/VolumePanelPositionController.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2025 Amaan Qureshi <contact@amaanq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.sound
+
+import android.content.Context
+import android.provider.Settings
+import com.android.settings.R
+import com.android.settings.core.TogglePreferenceController
+
+/**
+ * Preference controller for volume panel position.
+ *
+ * When enabled, the volume panel is displayed on the left side of the screen.
+ * This is useful for left-handed users.
+ */
+class VolumePanelPositionController(
+    context: Context,
+    key: String
+) : TogglePreferenceController(context, key) {
+
+    override fun getAvailabilityStatus(): Int = AVAILABLE
+
+    override fun isChecked(): Boolean =
+        Settings.Global.getInt(mContext.contentResolver, Settings.Global.VOLUME_PANEL_ON_LEFT, 0) == 1
+
+    override fun setChecked(isChecked: Boolean): Boolean =
+        Settings.Global.putInt(mContext.contentResolver, Settings.Global.VOLUME_PANEL_ON_LEFT, if (isChecked) 1 else 0)
+
+    override fun getSliceHighlightMenuRes(): Int = R.string.menu_key_sound
+}
