ffb49d8b4a94f24a2be765b7dfddbbcb852c9f59
Amaan Qureshi <git@amaanq.com>
2025-12-29 16:38:45 -0500

    feat: double tap to sleep UI

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -670,4 +670,5 @@ public class Settings extends SettingsActivity {
     public static class SafetyCenterActivity extends SettingsActivity { }
     public static class StatusBarSettingsActivity extends SettingsActivity { /* empty */ }
     public static class NetworkTrafficSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class DoubleTapSleepSettingsActivity extends SettingsActivity { /* empty */ }
 }
